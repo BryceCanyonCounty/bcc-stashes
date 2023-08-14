94b61942cf2eed401ed796acb9db9ac580faa4d3
BurntJannings <82676805+BurntJannings@users.noreply.github.com>
2023-08-13 18:46:16 -0600

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -6,18 +6,142 @@ end)
 
 local VORPInv = {}
 
+crypt = exports['bcc-crypt'].install()
+
+local stashid
+
 VORPInv = exports.vorp_inventory:vorp_inventoryApi()
 
-JobOnly, Jobs, JobGrades, BlacklistItems = nil, {}, nil, {}
+JobOnly, Jobs, JobGrades, BlacklistItems, Uuid = nil, {}, nil, {}, nil
+
+CreateThread(function()
+  for k, v in pairs(Config.Props) do
+    VORPInv.RegisterUsableItem(v.dbname, function(data)
+      TriggerClientEvent('bcc-stashes:PlaceContainer', data.source, v.ContainerName, v.hash)
+    end)
+  end
+end)
+
+RegisterNetEvent('bcc-stashes:registerInventory', function(id, hash)
+  if Config.Props[hash].Shared then
+    if Config.Props[hash].NotAllowedItems then
+      VORPInv.registerInventory(id, Config.Props[hash].ContainerName, Config.Props[hash].limit, true, true, true, false,
+        false, true, false)
+    else
+      VORPInv.registerInventory(id, Config.Props[hash].ContainerName, Config.Props[hash].limit, true, true, true, false,
+        false, false, false)
+    end
+  else
+    if Config.Props[hash].NotAllowedItems then
+      VORPInv.registerInventory(id, Config.Props[hash].ContainerName, Config.Props[hash].limit, true, false, true, false,
+        false, true, false)
+    else
+      VORPInv.registerInventory(id, Config.Props[hash].ContainerName, Config.Props[hash].limit, true, false, true, false,
+        false, false,
+        false)
+    end
+  end
+end)
+
+
+RegisterNetEvent('bcc-stashes:SaveToDB', function(name, hash, x, y, z, h)
+  Uuid = crypt.uuid4()
+  local param = {
+    ['id'] = Uuid,
+    ['name'] = name,
+    ['hash'] = hash,
+    ['x'] = x,
+    ['y'] = y,
+    ['z'] = z,
+    ['h'] = h
+  }
+
+  local db = MySQL.query.await(
+    "INSERT INTO stashes (`id`, `name`,`propname`,`x`,`y`,`z`,`h` ) VALUES ( @id,@name,@hash,@x,@y,@z,@h) RETURNING *;",
+    param)
+end)
+
+
+VORPcore.addRpcCallback('CreateStash', function(source, cb, args)
+  local _source = source
+  local character = VORPcore.getUser(_source).getUsedCharacter
+  local charId = character.charIdentifier
+  Uuid = crypt.uuid4()
+  local param = {
+    ['id'] = Uuid,
+    ['name'] = args.Name,
+    ['hash'] = args.Hash,
+    ['x'] = args.X,
+    ['y'] = args.Y,
+    ['z'] = args.Z,
+    ['h'] = args.H
+  }
+
+
+  -- args contains a table of stuff you're sending over from the client. (see args as the last argument in the RPC call.
+
+  local db = MySQL.query.await(
+    "INSERT INTO stashes (`id`, `name`,`propname`,`x`,`y`,`z`,`h` ) VALUES ( @id,@name,@hash,@x,@y,@z,@h) RETURNING *;",
+    param)
+  print(Uuid)
+  cb(db)
+end)
+
+
+VORPcore.addRpcCallback('GetStashes', function(source, cb)
+  local _source = source
+  local result = MySQL.query.await(
+    'SELECT * FROM stashes')
+  cb(result)
+end)
+
+RegisterNetEvent("bcc-stashes:OpenPropStash") -- inventory system
+AddEventHandler("bcc-stashes:OpenPropStash", function(containerid, JobNames)
+  local _source = source
+
+  local Character = VORPcore.getUser(_source).getUsedCharacter
+  local job = Character.job
+  local jobgrade = Character.jobGrade
+  for k, v in pairs(Config.Props) do
+    if v.NotAllowedItems then
+      for k, v in pairs(BlacklistItems) do
+        VORPInv.BlackListCustomAny(containerid, v)
+      end
+    end
+    Jobs = JobNames
+    JobGrades = v.JobGrades
+    BlacklistItems = v.Items
+    if v.JobOnly then
+      JobOnly = true
+    else
+      JobOnly = false
+    end
+  end
+  Wait(250)
+  if JobOnly then
+    if CheckTable(Jobs, job) then
+      if jobgrade >= JobGrades then
+        VORPInv.OpenInv(_source, containerid)
+        stashid = containerid
+      else
+        VORPcore.NotifyTip(_source, _U("WrongJobGrade"), 4000)
+      end
+    else
+      VORPcore.NotifyTip(_source, _U("WrongJob"), 4000)
+    end
+  else
+    VORPInv.OpenInv(_source, containerid)
+    stashid = containerid
+  end
+end)
 
 RegisterNetEvent("bcc-stashes:OpenContainer") -- inventory system
 AddEventHandler("bcc-stashes:OpenContainer", function(containerid, containername, limit, JobNames)
   local _source = source
-  print(containername)
   local Character = VORPcore.getUser(_source).getUsedCharacter
   local job = Character.job
   local jobgrade = Character.jobGrade
-  for k, v in pairs(Spots) do
+  for k, v in pairs(Config.Spots) do
     if v.NotAllowedItems then
       for k, v in pairs(BlacklistItems) do
         VORPInv.BlackListCustomAny(containerid, v)
@@ -47,10 +171,10 @@ AddEventHandler("bcc-stashes:OpenContainer", function(containerid, containername
   end
   Wait(250)
   if JobOnly then
-    print(json.encode(Jobs))
     if CheckTable(Jobs, job) then
       if jobgrade >= JobGrades then
         VORPInv.OpenInv(_source, containerid)
+        stashid = containerid
       else
         VORPcore.NotifyTip(_source, _U("WrongJobGrade"), 4000)
       end
@@ -59,21 +183,20 @@ AddEventHandler("bcc-stashes:OpenContainer", function(containerid, containername
     end
   else
     VORPInv.OpenInv(_source, containerid)
+    stashid = containerid
   end
 end)
 
+
 RegisterServerEvent("vorp_inventory:MoveToCustom", function(obj)
-  print('test')
   local _source = source
   local Character = VORPcore.getUser(_source).getUsedCharacter
   local item = json.decode(obj)
-
   VORPcore.AddWebhook(Config.WebhookInfo.Title, Config.WebhookInfo.Webhook,
     Character.firstname ..
-    " " .. Character.lastname .. _U("Moved") .. item.number .. " " .. item.item.name .. _U("ToStash"),
+    " " .. Character.lastname .. _U("Moved") .. item.number .. " " .. item.item.name .. _U("ToStash") .. stashid,
     Config.WebhookInfo.Color,
     Config.WebhookInfo.Name, Config.WebhookInfo.Logo, Config.WebhookInfo.FooterLogo, Config.WebhookInfo.Avatar)
-  print('finished moved')
 end)
 RegisterServerEvent("vorp_inventory:TakeFromCustom", function(obj)
   local _source = source
@@ -82,7 +205,7 @@ RegisterServerEvent("vorp_inventory:TakeFromCustom", function(obj)
 
   VORPcore.AddWebhook(Config.WebhookInfo.Title, Config.WebhookInfo.Webhook,
     Character.firstname ..
-    " " .. Character.lastname .. _U("Took") .. item.number .. " " .. item.item.name .. _U("ToStash"),
+    " " .. Character.lastname .. _U("Took") .. item.number .. " " .. item.item.name .. _U("ToStash") .. stashid,
     Config.WebhookInfo.Color,
     Config.WebhookInfo.Name, Config.WebhookInfo.Logo, Config.WebhookInfo.FooterLogo, Config.WebhookInfo.Avatar)
 end)
