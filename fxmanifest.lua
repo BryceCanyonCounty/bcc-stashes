ac857a64b049e2b320251c835a9c57ad710bdbc1
Andrew <10902965+AndrewR3K@users.noreply.github.com>
2023-10-06 16:39:48 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,10 @@
 fx_version "adamant"
-
 games { "rdr3" }
-
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+lua54 'yes'
+author 'Jannings'
+
+
 
 client_scripts {
 	'client/client.lua'
@@ -28,3 +30,5 @@ dependencies {
 	'vorp_utils',
 	'bcc-utils'
 }
+
+version '1.2.3'
