d134e17b181e184e8ba93546b8a738a4fb1f715a
Elzetia <cy.buniet@gmail.com>
2024-01-02 11:59:24 +0100

    Create fr.lua (#18)
    French translation by #slipknot4512

---
+++ shared/fr.lua
@@ -0,0 +1,9 @@
+Locales['fr'] = {
+  OpenStorage = 'Ouvrir le stockage',
+  Container = 'Conteneur',
+  WrongJobGrade = 'Grade de travail incorrect',
+  WrongJob = 'Travail incorrect',
+  Moved = ' a déplacé ',
+  Took = ' a pris ',
+  ToStash = ' dans le stockage '
+}
