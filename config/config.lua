58c67c6508197ffec4e3686f6b824e8b498d8201
Apollyon <37031311+JusCampin@users.noreply.github.com>
2024-12-08 23:43:00 -0800

    Update config.lua
    - fixed table layout for spot, props and webhooks
    - housekeeping

--- config/config.lua
+++ config/config.lua
@@ -1,10 +1,17 @@
 Config = {
-    DevMode = true, --False on live server
+    DevMode = false, --False on live server
+
     defaultlang = 'en_lang',
+
     keys = {
-        Pickup = "G", --[G] Open Chest prompt
-        Open = "B"
+        Pickup = "G", --Pickup Chest prompt
+        Open = "B"    -- Open Chest prompt
     },
+
+    WebhookEnable = false,
+    WebhookTitle = 'BCC-Stashes',
+    WebhookLink = '',
+    WebhookAvatar = '',
 
     Spots = {
         teststorage1 = {
@@ -12,7 +19,7 @@ Config = {
             containerid = "Testing2",                      --Unique Container ID
             ContainerName = "Stash",                       --Name shown in inventory UI
             limit = 250,                                   --Limit of storage
-            JobOnly = true,                                --Only job can access
+            JobOnly = false,                                --Only job can access
             JobName = { 'unemployed', 'offpolice' },       --Job names must go inside {} ie {'unemployed'} or {'police','sheriff'}
             JobGrades = 3,                                 -- If using Job Only, Job grade must be this rank or higher to access
             Shared = true,                                 -- Shared Storage
@@ -20,12 +27,13 @@ Config = {
             Items = { 'canteen' },                         --Blacklisted Items
             prophash = "p_chestmedhunt01x"                 -- Prop hash for the object
         },
+
         teststorage2 = {
             Pos = { x = -326.89, y = 775.05, z = 121.64 }, -- Coords in X,Y,Z
             containerid = "TestStorage2-2",                --Unique Container ID
             ContainerName = "test Storage",                --Name shown in inventory UI
             limit = 250,                                   --Limit of storage
-            JobOnly = true,                                --Only job can access
+            JobOnly = false,                                --Only job can access
             JobName = { 'sheriff', 'police' },             --Job names must go inside {} ie {'unemployed'} or {'police','sheriff'}
             JobGrades = 3,                                 -- If using Job Only, Job grade must be this rank or higher to access
             Shared = true,                                 -- Shared Storage
@@ -33,42 +41,37 @@ Config = {
             Items = {},                                    --Blacklisted Items
             prophash = "p_chestmedhunt01x"                 -- Prop hash for the object},
         },
-        Props = {
-            p_chest01x =                             -- hash of prop
-            {
-                dbname = 'chest1',                   ---Name of db item
-                hash = 'p_chest01x',                 -- hash of prop
-                containerid = "Stash2-1",            --Unique Container ID
-                ContainerName = "Stash1",            --Name shown in inventory UI
-                limit = 250,                         --Limit of storage
-                JobOnly = false,                     --Only job can access
-                JobName = { 'unemployed', 'offpolice' }, --Job names must go inside {} ie {'unemployed'} or {'police','sheriff'}
-                JobGrades = 3,                       -- If using Job Only, Job grade must be this rank or higher to access
-                Shared = true,                       -- Shared Storage
-                NotAllowedItems = true,              --Blacklist below items, Not in use yet
-                Items = { 'canteen' }
-            },
+    },
 
-            p_chest02x = -- hash of prop
-
-            {
-                dbname = 'chest2',                   ---Name of db item
-                hash = 'p_chest02x',                 -- hash of prop
-                containerid = "Stash2-2",            --Unique Container ID
-                ContainerName = "Stash2",            --Name shown in inventory UI
-                limit = 250,                         --Limit of storage
-                JobOnly = false,                     --Only job can access
-                JobName = { 'unemployed', 'offpolice' }, --Job names must go inside {} ie {'unemployed'} or {'police','sheriff'}
-                JobGrades = 3,                       -- If using Job Only, Job grade must be this rank or higher to access
-                Shared = true,                       -- Shared Storage
-                NotAllowedItems = true,              --Blacklist below items, Not in use yet
-                Items = { 'canteen' }
-            },
+    Props = {
+        p_chest01x =                                 -- hash of prop
+        {
+            dbname = 'chest1',                       ---Name of db item
+            hash = 'p_chest01x',                     -- hash of prop
+            containerid = "Stash2-1",                --Unique Container ID
+            ContainerName = "Stash1",                --Name shown in inventory UI
+            limit = 250,                             --Limit of storage
+            JobOnly = false,                         --Only job can access
+            JobName = { 'unemployed', 'offpolice' }, --Job names must go inside {} ie {'unemployed'} or {'police','sheriff'}
+            JobGrades = 3,                           -- If using Job Only, Job grade must be this rank or higher to access
+            Shared = true,                           -- Shared Storage
+            NotAllowedItems = true,                  --Blacklist below items
+            Items = { 'canteen' }
         },
 
-        WebhookEnable = true,
-        WebhookTitle = 'BCC-Stashes',
-        WebhookLink = '',
-        WebhookAvatar = ''
+        p_chest02x =                                 -- hash of prop
+        {
+            dbname = 'chest2',                       ---Name of db item
+            hash = 'p_chest02x',                     -- hash of prop
+            containerid = "Stash2-2",                --Unique Container ID
+            ContainerName = "Stash2",                --Name shown in inventory UI
+            limit = 250,                             --Limit of storage
+            JobOnly = false,                         --Only job can access
+            JobName = { 'unemployed', 'offpolice' }, --Job names must go inside {} ie {'unemployed'} or {'police','sheriff'}
+            JobGrades = 3,                           -- If using Job Only, Job grade must be this rank or higher to access
+            Shared = true,                           -- Shared Storage
+            NotAllowedItems = true,                  --Blacklist below items
+            Items = { 'canteen' }
+        },
     }
 }
